738effb26cfb1a8c2d974ce150f2476e321acef2
Mateo <masprime77@MacBook-Pro-de-Mate.local>
2024-03-13 08:38:12 +0100

    Update test1.py: Corrct sintaxis

--- test1.py
+++ test1.py
@@ -4,4 +4,5 @@ for i in range(1, 6):
     print("Hello for the {} time".format(i))
     if i < 3:
         print("I'm under 3.")
-    else: print("I'm over or 3.")
+    else:
+        print("I'm over or 3.")
