aa56faddd5a8bcad0043f240554254c48fada2a0
Mateo <masprime77@MacBook-Pro-de-Mate.local>
2024-03-13 08:30:27 +0100

    Update test1.py: update if else statement

--- test1.py
+++ test1.py
@@ -3,4 +3,5 @@ print("Hello")
 for i in range(1, 6):
     print("Hello for the {} time".format(i))
     if i < 3:
-        print("I'm under 3")
+        print("I'm under 3.")
+    else: print("I'm over or 3.")
